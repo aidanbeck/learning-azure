672b8428b31c23896a73adbd9dec1004222043ea
Aidan Beck <158112309+aidanbeck@users.noreply.github.com>
2026-09-14 21:33:21 +0000

    initial project setup

---
+++ MyFunctionApp/HelloWorld.cs
@@ -0,0 +1,23 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFunctionApp;
+
+public class HelloWorld
+{
+    private readonly ILogger<HelloWorld> _logger;
+
+    public HelloWorld(ILogger<HelloWorld> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("GetUserInfo")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Aidan Beck");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # learning-azure
 Learning Azure Function Apps
+
+## Command line setup (For Codespaces)
+* npm install -g azure-functions-core-tools@4 --unsafe-perm true
+* func init MyFunctionApp --worker-runtime dotnet-isolated
+* cd MyFunctionApp
+* func new (Choose HttpTrigger)
+* func start
+    * OR: dotnet run
